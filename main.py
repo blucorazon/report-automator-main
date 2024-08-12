9e20d7a390a6a924eabb09930b84310bdb8605a4
Blu <143293307+blucorazon@users.noreply.github.com>
2024-08-12 17:55:10 -0400

    Refactor main program flow; edit log statements for more consistency

--- main.py
+++ main.py
@@ -31,15 +31,14 @@ def main(start_year, end_year, term_number, mode):
     Returns:
         None
     """
-    # Set header for logging
-    start_time = log_run_header()
+    start_time = log_run_header() # Set header for logging
 
     # Create parent destination for reports
     folder_id = create_destination_folder(start_year, end_year, term_number) # Parent directory for created reports
-    source_file_id = "1mu6gW8FvtZ1xg6u9Is1BSJVlbF0UUqL_LZcOq2Z8ALc" # Location of report template
+    source_file_id = "1mu6gW8FvtZ1xg6u9Is1BSJVlbF0UUqL_LZcOq2Z8ALc" # Location of report template in Google Drive
 
     # Retrieve student data from database
-    logger.debug("# Retreive student data from database")
+    logger.debug("# Retreiving student data from database")
     if mode == "test":
         # TODO: Implement test table in database.py module
         students_data = readTestTable()
@@ -47,30 +46,27 @@ def main(start_year, end_year, term_number, mode):
         students_data = readSqliteTable()
     
     # Create a dictionary to track course folders
-    logger.debug("# Create a dictionary to track course folders")
+    logger.debug("# Creating a dictionary to track course folders")
     course_folders = {}
 
     if students_data:
-      for record in students_data:
-          last_name, first_name, course_name = record
+        for last_name, first_name, course_name in students_data:
+            # Check if course folder exists; if not, create it
+            course_folder_id = course_folders.get(course_name)
 
-          # Check if course folder has been created, if not, create it
-          if course_name not in course_folders:
-               course_folder_id = create_course_folder(course_name, folder_id)
-               course_folders[course_name] = course_folder_id
-               logger.info(f"Folder: '{course_name}' successfully created in parent directory.")
-          else:
-               course_folder_id = course_folders[course_name]
+            # If it doesn't exist, create it and store the ID
+            if not course_folder_id:
+                course_folder_id = create_course_folder(course_name, folder_id)
+                course_folders[course_name] = course_folder_id
+                logger.info(f"Folder: '{course_name}' successfully created in parent directory.")
 
-          # Copy the template for current student
-          logger.debug("# Copy the template for current student")
-          unformatted_report_id = copy_template(course_folder_id, source_file_id)
+            # Copy the template for current student
+            logger.debug("# Copying the template for current student")
+            unformatted_report_id = copy_template(course_folder_id, source_file_id)
 
-          # Format the new title
-          formatted_title = f"{last_name}, {first_name} ({course_name})"
-                
-          # Update the title of the copied document
-          format_document_title(unformatted_report_id, formatted_title)
+            # Format the new title
+            formatted_title = f"{last_name}, {first_name} ({course_name})"
+            format_document_title(unformatted_report_id, formatted_title)
     logger.info("Reports generated and sorted successfully.")
     
     # Record the end time
