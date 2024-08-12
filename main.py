5472257127a62c119b5706af3cfd7b2861087071
Blu <143293307+blucorazon@users.noreply.github.com>
2024-08-12 18:15:22 -0400

    Clean up extra spaces and formatting; Refactor dependency management at top of file

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import argparse
 from datetime import datetime
-from module2.drive_api import copy_template
-from module2.drive_api import create_course_folder
-from module2.drive_api import create_destination_folder
-from module2.drive_api import format_document_title
-from module4.database import readSqliteTable
-from module4.database import readTestTable
-from module5.utils import logger
-from module5.utils import log_run_header
+import module2.drive_api as drive_api
+import module4.database as database
+import module5.utils as utils
 
 def main(start_year, end_year, term_number, mode):
     """ Generate and organize student report templates.
@@ -31,22 +26,22 @@ def main(start_year, end_year, term_number, mode):
     Returns:
         None
     """
-    start_time = log_run_header() # Set header for logging
+    start_time = utils.log_run_header() # Set header for logging
 
     # Create parent destination for reports
-    folder_id = create_destination_folder(start_year, end_year, term_number) # Parent directory for created reports
+    folder_id = drive_api.create_destination_folder(start_year, end_year, term_number) # Parent directory for created reports
     source_file_id = "1mu6gW8FvtZ1xg6u9Is1BSJVlbF0UUqL_LZcOq2Z8ALc" # Location of report template in Google Drive
 
     # Retrieve student data from database
-    logger.debug("# Retreiving student data from database")
+    utils.logger.debug("# Retreiving student data from database")
     if mode == "test":
         # TODO: Implement test table in database.py module
-        students_data = readTestTable()
+        students_data = database.readTestTable()
     else:
-        students_data = readSqliteTable()
+        students_data = database.readSqliteTable()
     
     # Create a dictionary to track course folders
-    logger.debug("# Creating a dictionary to track course folders")
+    utils.logger.debug("# Creating a dictionary to track course folders")
     course_folders = {}
 
     if students_data:
@@ -56,25 +51,25 @@ def main(start_year, end_year, term_number, mode):
 
             # If it doesn't exist, create it and store the ID
             if not course_folder_id:
-                course_folder_id = create_course_folder(course_name, folder_id)
+                course_folder_id = drive_api.create_course_folder(course_name, folder_id)
                 course_folders[course_name] = course_folder_id
-                logger.info(f"Folder: '{course_name}' successfully created in parent directory.")
+                utils.logger.info(f"Folder: '{course_name}' successfully created in parent directory.")
 
             # Copy the template for current student
-            logger.debug("# Copying the template for current student")
-            unformatted_report_id = copy_template(course_folder_id, source_file_id)
+            utils.logger.debug("# Copying the template for current student")
+            unformatted_report_id = drive_api.copy_template(course_folder_id, source_file_id)
 
             # Format the new title
             formatted_title = f"{last_name}, {first_name} ({course_name})"
-            format_document_title(unformatted_report_id, formatted_title)
-    logger.info("Reports generated and sorted successfully.")
+            drive_api.format_document_title(unformatted_report_id, formatted_title)
+    utils.logger.info("Reports generated and sorted successfully.")
     
     # Record the end time
     end_time = datetime.now()
     duration = end_time - start_time
 
     # Log and Print total run time
-    logger.info(f"Total run time: {duration}")
+    utils.logger.info(f"Total run time: {duration}")
 
 if __name__ == "__main__":    
     # Parse command-line arguments
@@ -85,8 +80,6 @@ if __name__ == "__main__":
     parser.add_argument("mode", type=str, nargs="?", default="normal", choices=["normal", "test"],
                         help="Mode of operation (normal or test)")
     args = parser.parse_args()
-
-    # TODO Preprocess the database
 
     # Main program
     main(args.start_year, args.end_year, args.term_number, args.mode)
